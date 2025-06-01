d989d7bba8c586109e4492bbc798a84c0c99b188
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-06-01 22:59:30 +0700

    feat: add button theme toggle can swap between moon and sun theme

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-background text-foreground overflow-hidden">
       {/* Theme Toggle */}
+      <ThemeToggle />
 
       {/* Background Effects */}
 
