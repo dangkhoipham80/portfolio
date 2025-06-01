2d07e137a5ac08b51f2b7987fda1c50f6e959a46
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-06-01 22:50:49 +0700

    feat: add full index.css

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
 };
 
 export default Home;
