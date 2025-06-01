8a19f61468ba58b10a13a610245e2a9aab9aea94
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-06-02 00:47:36 +0700

    feat: navbar

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <StarBackground />
 
       {/* Navbar */}
+      <Navbar />
 
       {/* Main Content */}
 
